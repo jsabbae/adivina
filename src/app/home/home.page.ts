1ba1105502938e96b5dfefb09ffde47d41fe884a
jsabbae2702 <jsabbae2702@g.educaand.es>
2024-10-23 14:53:40 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  num: number = 0;
+  numSecret: number = this.numAleatorio(1, 100);
+  mayorMenor: string = '...';
 
+  constructor() { 
+    console.log("El número secreto es: "+ this.numSecret);
+  }
+  numAleatorio(a, b) {
+    return Math.round(Math.random() * (b - a) + parseInt(a));
+  }
+  compruebaNumero() {
+    if (this.num) {
+      if (this.numSecret < this.num) {
+        this.mayorMenor = 'menor';
+      } else if (this.numSecret > this.num) {
+        this.mayorMenor = 'mayor';
+      } else {
+        this.mayorMenor = 'igual';
+      }
+    }
+  }
 }
